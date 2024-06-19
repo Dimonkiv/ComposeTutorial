5d62b765659d13c46c018fec1cfba1443832b1ff
Dimonkiv <iw97dimon@gmail.com>
2024-06-19 16:36:47 +0300

    Constraint layout

--- app/src/main/java/com/dimonkiv/composetutorial/MainActivity.kt
+++ app/src/main/java/com/dimonkiv/composetutorial/MainActivity.kt
@@ -3,11 +3,53 @@ package com.dimonkiv.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val constraints = ConstraintSet {
+                val firstBox = createRefFor("firstBox")
+                val secondBox = createRefFor("secondBox")
+
+                constrain(firstBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                constrain(secondBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(firstBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.value(100.dp)
+                }
+            }
+            
+            ConstraintLayout(constraintSet = constraints, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier
+                    .background(Color.Red)
+                    .layoutId("firstBox")
+                )
+
+                Box(modifier = Modifier
+                    .background(Color.Blue)
+                    .layoutId("secondBox")
+                )
+                
+            }
 
         }
     }
