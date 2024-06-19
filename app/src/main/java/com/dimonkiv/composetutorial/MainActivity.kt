ca407c64880f2d637fe8712b25161e6731cf4e5f
Dimonkiv <iw97dimon@gmail.com>
2024-06-19 10:34:36 +0300

    Implement Image card UI

--- app/src/main/java/com/dimonkiv/composetutorial/MainActivity.kt
+++ app/src/main/java/com/dimonkiv/composetutorial/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,8 +15,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -25,86 +22,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColorBox(modifier = Modifier.fillMaxSize())
+            val painter = painterResource(id = R.drawable.image)
+            val contentDescription = "This is description"
+            val text = "This is my first Jetpack sample."
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(5.dp)
+            ) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    text = text
+                )
+            }
         }
     }
-}
-
-@Composable
-fun ColorBox(modifier: Modifier = Modifier) {
-    val color = remember {
-        mutableStateOf(Color.Yellow)
-    }
-
-    Box(modifier = modifier
-        .background(color.value)
-        .clickable {
-            color.value = Color(
-                Random.nextFloat(),
-                Random.nextFloat(),
-                Random.nextFloat(),
-                1f
-            )
-        }
-    )
-}
-
-@Composable
-fun StyledText(text: String) {
-    val fontFamily = FontFamily(
-        Font(R.font.font_thin, FontWeight.Thin),
-        Font(R.font.font_light, FontWeight.Light),
-        Font(R.font.font_extra_light, FontWeight.ExtraLight),
-        Font(R.font.font_regular, FontWeight.Normal)
-    )
-    Text(
-        text = buildAnnotatedString {
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Blue,
-                    fontSize = 45.sp
-                )
-            ) {
-                append("J")
-            }
-            append("epack ")
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Blue,
-                    fontSize = 45.sp
-                )
-            ) {
-                append("C")
-            }
-            append("ompose")
-        },
-        color = Color.White,
-        fontSize = 30.sp,
-        fontStyle = FontStyle.Italic,
-        fontFamily = fontFamily,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center,
-        textDecoration = TextDecoration.Underline
-    )
 }
 
 @Composable
@@ -154,37 +96,5 @@ fun ImageCard(
 
         }
 
-    }
-}
-
-@Composable
-@Preview
-fun ImageCardPreview() {
-    val painter = painterResource(id = R.drawable.image)
-    val contentDescription = "This is description"
-    val text = "This is my first Jetpack sample."
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth(0.5f)
-            .padding(5.dp)
-    ) {
-        ImageCard(
-            painter = painter,
-            contentDescription = contentDescription,
-            text = text
-        )
-    }
-}
-
-@Composable
-@Preview
-fun StyledTextPreview() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black),
-    ) {
-        StyledText(text = "Jetpack Compose")
     }
 }
